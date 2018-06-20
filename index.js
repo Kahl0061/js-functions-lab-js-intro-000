006b053d4e4664d6f663cac350c3d09f667fa267
Robbie Kahl <rob.kahl@target.com>
2018-06-20 15:38:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
+function happyHolidaysTo(name){
+  return "Happy Holidays, ${name}!"
+}
+
+function happyHolidaysTo(holiday, name){
+  return "Happy " + holiday + ", " + namve + "!"
+}
+
+function holidayCountdown(holiday, days){
+  return "It\'s " + days + " unitil " + holiday
+}
